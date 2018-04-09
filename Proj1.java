abd9e5f1c6f2c8900071baac5d66e452cfec4943
Tres27 <35712557+Tres27@users.noreply.github.com>
2018-04-09 06:27:13 -0700

    Add files via upload

---
+++ Proj1.java
@@ -0,0 +1,35 @@
+
+/**
+ * Ascii Art
+ * Chaliha CatFace
+ *
+ * Shreyas Chaliha
+ * Project 1 
+ * http://www.asciiworld.com/-Cats-.html
+ */
+public class Proj1
+{
+    public static void main(String[] args)  
+    {
+     System.out.println("                        $                         $");
+     System.out.println("                      $  $                       $  $");
+     System.out.println("                     $    $---------------------$    $");
+     System.out.println("                    O                                  O"); 
+     System.out.println("                   O                                    O"); 
+     System.out.println("                  O      $$$               $$$           O"); 
+     System.out.println("                  O     $ ? $             $ ? $          O"); 
+     System.out.println("                  O      $$$               $$$          O"); 
+     System.out.println("                  O                                     O"); 
+     System.out.println("                  O                                    O"); 
+     System.out.println("                   0              ccccc               0"); 
+     System.out.println("                    O            c$ $ $c             O"); 
+     System.out.println("                     O          ccccccccc           O"); 
+     System.out.println("                      O                            O"); 
+     System.out.println("                       O      --------------     O"); 
+     System.out.println("                        O                      O"); 
+     System.out.println("                         O                    O"); 
+     System.out.println("                          O                 O"); 
+     System.out.println("                           O000000000000000O"); 
+      
+    }
+}
